b873a61cdb4a539907e2f0deb351afe5a68de95f
tupd2301 <131238772+tupd2301@users.noreply.github.com>
2024-07-17 07:52:47 +0700

    upgrade to urp

--- Assets/Project/Scripts/PetManager.cs
+++ Assets/Project/Scripts/PetManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PetManager : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> petModels = new List<GameObject>();
+    [SerializeField] private List<Pet> petModels = new List<Pet>();
     [SerializeField] private GameObject prefab;
 
     public void SpawnPet()
@@ -12,5 +12,21 @@ public class PetManager : MonoBehaviour
         GameObject obj = Instantiate(prefab, transform);
         obj.transform.localPosition = new Vector3(0, 0, 0);
         obj.transform.localEulerAngles = new Vector3(0, 0, 0);
+    }
+}
+[System.Serializable]
+public class Pet
+{
+    public int id;
+    public GameObject model;
+    public Vector3 position;
+    public int rotationY;
+
+    public Pet(int id, GameObject model, Vector3 position, int rotationY)
+    {
+        this.id = id;
+        this.model = model;
+        this.position = position;
+        this.rotationY = rotationY;
     }
 }
