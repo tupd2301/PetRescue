fc9119a495cb8c17b8f2e553e0e21f5daa854ed8
tupd2301 <131238772+tupd2301@users.noreply.github.com>
2024-07-24 14:39:27 +0700

    update

--- Assets/PetRescue/Project/Scripts/SpecialTiles/SpecialTileBoom.cs
+++ Assets/PetRescue/Project/Scripts/SpecialTiles/SpecialTileBoom.cs
@@ -8,6 +8,14 @@ public class SpecialTileBoom : MonoBehaviour
     {
         List<BaseData> list = GamePlay.Instance.baseManager.GetBaseNearByCoordinates(GetComponent<BaseComponent>().baseData.coordinates);
         list.Add(GetComponent<BaseComponent>().baseData);
+        foreach (var item in list)
+        {
+            if(GamePlay.Instance.petManager.CheckPetExist(item.coordinates))
+            {
+                PetComponent petComponent = GamePlay.Instance.petManager.GetPetByCoordinates(item.coordinates).petComponent;
+                petComponent.Next(item, petComponent.transform.position, true);
+            }
+        }
         StartCoroutine(GamePlay.Instance.baseManager.SinkBases(list, 0.1f));
     }
 }
